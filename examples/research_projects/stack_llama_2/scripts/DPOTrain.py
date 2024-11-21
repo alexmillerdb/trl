462b6b2b1cb43f304d186e64f7a96e17a1f3bd7d
alexmillerdb <alex.miller@databricks.com>
2024-11-21 15:42:22 -0700

    update to bf16

--- examples/research_projects/stack_llama_2/scripts/DPOTrain.py
+++ examples/research_projects/stack_llama_2/scripts/DPOTrain.py
@@ -7,6 +7,6 @@ model = AutoModelForCausalLM.from_pretrained("Qwen/Qwen2-0.5B-Instruct")
 tokenizer = AutoTokenizer.from_pretrained("Qwen/Qwen2-0.5B-Instruct")
 train_dataset = load_dataset("trl-lib/ultrafeedback_binarized", split="train")
 
-training_args = DPOConfig(output_dir="Qwen2-0.5B-DPO", logging_steps=10, ddp_timeout=3600, auto_find_batch_size=True)
+training_args = DPOConfig(output_dir="Qwen2-0.5B-DPO", logging_steps=10, ddp_timeout=3600, auto_find_batch_size=True, bf16=True)
 trainer = DPOTrainer(model=model, args=training_args, processing_class=tokenizer, train_dataset=train_dataset)
 trainer.train()
