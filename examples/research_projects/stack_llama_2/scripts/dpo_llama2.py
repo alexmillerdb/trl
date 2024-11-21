2926ef3fe8b1a6ad82fbdc5add0c45033dbbfe65
alexmillerdb <alex.miller@databricks.com>
2024-11-21 10:31:53 -0700

    update dpo trainer args

--- examples/research_projects/stack_llama_2/scripts/dpo_llama2.py
+++ examples/research_projects/stack_llama_2/scripts/dpo_llama2.py
@@ -235,13 +235,13 @@ if __name__ == "__main__":
         model,
         ref_model=None,
         args=training_args,
-        beta=script_args.beta,
+        # beta=script_args.beta,
         train_dataset=train_dataset,
         eval_dataset=eval_dataset,
         processing_class=tokenizer,
         peft_config=peft_config,
-        max_prompt_length=script_args.max_prompt_length,
-        max_length=script_args.max_length,
+        # max_prompt_length=script_args.max_prompt_length,
+        # max_length=script_args.max_length,
     )
 
     # 6. train
